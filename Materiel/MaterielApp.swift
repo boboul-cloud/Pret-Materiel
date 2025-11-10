cbd05f6a4e6a4125c0652a2f06af692291ce39c2
BudgetApp <bob.oulhen@gmail.com>
2025-11-10 12:05:56 +0100

    Initial Commit

---
+++ Materiel/MaterielApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaterielApp.swift
+//  Materiel
+//
+//  Created by Robert Oulhen on 10/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MaterielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
